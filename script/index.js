79c1ae6ad7fef93d03820899291cdbed0caa1a21
Divyansh Kumar <divyansh14kr@gmail.com>
2023-10-01 19:22:34 +0530

    updated the "get started" button in index.html to scroll to course cards section.

--- script/index.js
+++ script/index.js
@@ -1,7 +1,17 @@
-if (!(JSON.parse( localStorage.getItem("profileDetails")))) {
+if (!(JSON.parse(localStorage.getItem("profileDetails")))) {
   document.getElementById('question-div').innerHTML = '<a href="./public/Sign-up.html"><button class="profile-button">Sign In</button></a>';
 }
 
 function deleteContributionRequest() {
   document.querySelector(".contribution-request").remove();
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const getStartedButton = document.querySelector('.cta-button');
+    const courseCardsSection = document.getElementById('course-cards-section');
+
+    getStartedButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        courseCardsSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    });
+});
