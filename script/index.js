a8ba7cb3078b7aee83f25b8a3d8ce8906c9c8351
GbCderr14 <114488917+GbCderr14@users.noreply.github.com>
2023-10-01 20:33:27 +0530

    refactor:improved style of the menu to appear on search

--- script/index.js
+++ script/index.js
@@ -7,15 +7,11 @@ function deleteContributionRequest() {
   document.querySelector(".contribution-request").remove();
 }
 
-// Get references to the search input, course cards, course list container, and search button
 const searchInput = document.getElementById("search-input");
 const courseCards = document.querySelectorAll(".course-card");
 const courseList = document.getElementById("course-list");
 const searchButton = document.getElementById("search-button");
 
-// Function to filter courses based on search input
-
-// Add an input event listener to the search input
 searchInput.addEventListener("input", function () {
   const searchText = searchInput.value.toLowerCase();
 
@@ -24,20 +20,16 @@ searchInput.addEventListener("input", function () {
     return;
   }
 
-  // Clear the course list container
   courseList.innerHTML = "";
 
-  // Iterate over the course cards
   courseCards.forEach((card) => {
     const courseTitle = card.querySelector("h2").innerText.toLowerCase();
     const cardVisible = courseTitle.includes(searchText);
 
     if (cardVisible) {
-      // Create a list item for the matching course title
       const listItem = document.createElement("li");
       listItem.textContent = card.querySelector("h2").innerText;
-
-      // Add a click event listener to trigger the "Learn More" link click
+      listItem.style.listStyle = "none";
       listItem.addEventListener("click", function () {
         const learnMoreButton = card.querySelector(".learn-more-button");
         if (learnMoreButton) {
@@ -45,8 +37,6 @@ searchInput.addEventListener("input", function () {
         }
         searchInput.value = "";
       });
-
-      // Append the list item to the course list container
       courseList.appendChild(listItem);
     }
   });
@@ -55,10 +45,8 @@ searchInput.addEventListener("input", function () {
 // Add a click event listener to the search button
 searchButton.addEventListener("click", function () {
   const searchText = searchInput.value.toLowerCase();
-  // Clear the course list container
   courseList.innerHTML = "";
 
-  // Iterate over the course cards
   courseCards.forEach((card) => {
     const courseTitle = card.querySelector("h2").innerText.toLowerCase();
     const cardVisible = courseTitle.includes(searchText);
